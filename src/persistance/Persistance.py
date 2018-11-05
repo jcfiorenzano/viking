489e39d328e2162ea892d9b57dc2bbc0e614f6bf
Juan Carlos Fiorenzano <jcfiorenzano23@gmail.com>
2018-11-04 17:14:29 -0800

    Created file to persist data

--- src/persistance/Persistance.py
+++ src/persistance/Persistance.py
@@ -1,10 +1,17 @@
+import os
 from src.model import LoginInfo
-
+import pickle
 
 class Persistance:
+    def __init__(self):
+        self.FILE_NAME = "viking.bin"
+        self.path = os.path.dirname(__file__)+"/../"+self.FILE_NAME
 
     def save(self, login):
-        print(login.site+" "+login.username+" "+str(login.password, encoding="utf8"))
+        PYTHON_BINARY_PROTOCOL = 3
+
+        with open(self.path, 'ab') as passwordFile:
+            pickle.dump(login, passwordFile, PYTHON_BINARY_PROTOCOL)
 
     def load(self):
         return LoginInfo()
