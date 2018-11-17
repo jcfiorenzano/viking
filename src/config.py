a5c1c90b12f81234e3675e6caa9ea95c4a64706a
Juan Carlos Fiorenzano <jcfiorenzano23@gmail.com>
2018-11-17 11:10:17 -0800

    Added configuration file

---
+++ src/config.py
@@ -0,0 +1,3 @@
+import os
+
+VIKING_FILE_PATH = os.path.dirname(__file__) + "/viking.vik"
--- src/persistance/Persistance.py
+++ src/persistance/Persistance.py
@@ -1,20 +1,18 @@
-import os
 import pickle
+import src.config as config
 
 class Persistance:
     def __init__(self):
-        self._FILE_NAME = "viking.bin"
-        self._path = os.path.dirname(__file__) + "/../" + self._FILE_NAME
         self._SERIALIZE_PICKLE_PROTOCOL = 3
 
     def save(self, login):
-        with open(self._path, 'wb') as passwordFile:
+        with open(config.VIKING_FILE_PATH, 'wb') as passwordFile:
             login_dictionary = self.load()
             login_dictionary[login.site] = login
             pickle.dump(login_dictionary, passwordFile, self._SERIALIZE_PICKLE_PROTOCOL)
 
     def load(self):
-        with open(self._path, 'rb') as passwordFile:
+        with open(config.VIKING_FILE_PATH, 'rb') as passwordFile:
             try:
                 return pickle.load(passwordFile)
             except EOFError:
